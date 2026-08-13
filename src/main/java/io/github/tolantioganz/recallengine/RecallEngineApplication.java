f303fe08500c83ba6427ec816ee767f532bd6d82
Tolantio Ganzi <172088474+TolantioGanzi@users.noreply.github.com>
2026-08-12 21:00:46 -0400

    Initial Spring Boot scaffold

---
+++ src/main/java/io/github/tolantioganz/recallengine/RecallEngineApplication.java
@@ -0,0 +1,13 @@
+package io.github.tolantioganz.recallengine;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RecallEngineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RecallEngineApplication.class, args);
+	}
+
+}
